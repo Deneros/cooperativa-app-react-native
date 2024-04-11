ee5033f095f9657dc296609b57c4af64e81e1262
Nicolas Velez <nicolas.velez99@hotmail.com>
2024-04-10 20:08:40 -0500

    [feat]: componente personalizable de texto

---
+++ src/components/StyledText.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+    text:{
+        color: theme.colors.textPrimary,
+        fontSize: theme.fontSizes.body,
+        fontFamily: theme.fonts.main,
+        fontWeight: theme. fontWeights.normal
+    },
+    colorPrimary:{
+        color: theme.colors.primary
+    },
+    colorSecundary:{
+        color: theme.colors.textSecondary
+    },
+    bold:{
+        fontWeight: theme.fontWeights.bold
+    },
+    subHeading:{
+        fontSize: theme.fontSizes.subHeading
+    },
+    textAlignCenter:{
+        textAlign: 'center'
+    }
+
+})
+
+export default function StyledText({children, color, fontSize, align, fontWeight, style, ...restOfProps }) {
+    const textStyles = [
+        styles.text,
+        color === 'primary' && styles.colorPrimary,
+        color === 'secondary' && styles.colorSecundary,
+        fontSize === 'subHeading' && styles.subHeading,
+        fontWeight === 'bold' && styles.bold,
+        align === 'center' && styles.textAlignCenter,
+        style
+    ]
+
+    return (
+        <Text style={textStyles}{...restOfProps}> 
+            {children}
+        
+        </Text>
+    )
+}
