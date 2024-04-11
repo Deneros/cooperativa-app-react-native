10d8f5d2a89fffa57c96993f7084616c7aa705c2
Nicolas Velez <nicolas.velez99@hotmail.com>
2024-04-10 20:06:06 -0500

    [feat]: data prueba

---
+++ src/data/advisers.js
@@ -0,0 +1,31 @@
+export default [
+    {
+      "id": 1,
+      "name": "John Doe",
+      "email": "john.doe@example.com",
+      "position": "Senior Adviser",
+      "department": "Finance"
+    },
+    {
+      "id": 2,
+      "name": "Jane Smith",
+      "email": "jane.smith@example.com",
+      "position": "Financial Analyst",
+      "department": "Finance"
+    },
+    {
+      "id": 3,
+      "name": "Mike Johnson",
+      "email": "mike.johnson@example.com",
+      "position": "Marketing Specialist",
+      "department": "Marketing"
+    },
+    {
+      "id": 4,
+      "name": "Anna Williams",
+      "email": "anna.williams@example.com",
+      "position": "HR Manager",
+      "department": "Human Resources"
+    }
+  ]
+  
---
+++ src/data/products.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        "id": 'ca',
+        "product": "Cuenta de Ahorros",
+        "accountNumber": "123456789",
+        "balance": 5000.0,
+        "currency": "USD"
+    },
+    {
+        "id": 'cc',
+        "product": "Cuenta Corriente",
+        "accountNumber": "987654321",
+        "balance": 10000.0,
+        "currency": "USD"
+    },
+    {
+        "id": 'tc',
+        "product": "Tarjeta de Crédito - Libre Inversión",
+        "cardNumber": "**** **** **** 1234",
+        "creditLimit": 15000.0,
+        "currency": "USD"
+    },
+    {
+        "id": 'ch',
+        "product": "Crédito Hipotecario",
+        "loanNumber": "987654",
+        "loanAmount": 200000.0,
+        "remainingBalance": 180000.0,
+        "currency": "USD"
+    }
+]
+
