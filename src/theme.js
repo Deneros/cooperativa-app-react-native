cd3307ae95a7e27e23d077057fc158ba40751e4a
Nicolas Velez <nicolas.velez99@hotmail.com>
2024-04-10 20:06:35 -0500

    [feat]: se crea theme para manejo de constantes de estilo

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+const theme = {
+    colors: {
+        textPrimary: '#24292e',
+        textSecondary: '#586069',
+        primary: '#8CBD3A',
+        secondary: '#25873B',
+        third: '#D3DB2C'
+    },
+    fontSizes:{
+        body:14, 
+        subHeading:16,
+        title:24
+    },
+    fonts:{
+        main: 'System'
+    },
+    fontWeights:{
+        normal:'400',
+        bold: '700'
+    }
+}
+
+export default theme
